6257270233c046374a0207f123abd531b79b2e15
hanbing <hanbing@hanbingdeMacBook-Air.local>
2020-08-16 19:20:13 +0800

    还原读取数据库配置

--- Chapter12/src/test/java/con/course/code/utils/DatabaseUtils.java
+++ Chapter12/src/test/java/con/course/code/utils/DatabaseUtils.java
@@ -12,7 +12,7 @@ public class DatabaseUtils {
 	
 	public static SqlSession getSqlSession() throws IOException {
         //获取配置的资源文件
-        Reader reader = Resources.getResourceAsReader("/databaseConfig.xml");
+        Reader reader = Resources.getResourceAsReader("databaseConfig.xml");
         //得到SqlSessionFactory，使用类加载器加载xml文件
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader);
         //得到sqlsession对象，这个对象就能执行配置文件中的sql语句啦
